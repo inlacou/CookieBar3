33515140194e9db4c5fde956081c1a4fe475fa4e
Inlacou <inlacou@gmail.com>
2020-03-05 15:55:40 +0100

    more examples

--- app/src/main/java/com/inlacou/cookiebarsample/MainActivity.kt
+++ app/src/main/java/com/inlacou/cookiebarsample/MainActivity.kt
@@ -3,9 +3,12 @@ package com.inlacou.cookiebarsample
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.Gravity
 import android.view.View
+import android.view.animation.AccelerateDecelerateInterpolator
 import android.widget.Button
 import com.inlacou.cookiebar3.CookieBar.Companion.BOTTOM
+import com.inlacou.cookiebar3.CookieAnimation
 import com.inlacou.cookiebar3.CookieBar.Companion.build
 import com.inlacou.cookiebar3.CookieBar.Companion.dismiss
 
@@ -20,6 +23,21 @@ class MainActivity : AppCompatActivity() {
 					.setTitleColor(R.color.yellow)
 					.setMessage(R.string.top_cookie_message)
 					.setIcon(R.drawable.ic_android_white_48dp)
+					.setSteps(listOf(
+							AnimationStep(0, null, 0, CookieAnimation({
+								centerInParent(horizontal = true, vertical = false)
+								invisible()
+							})),
+							AnimationStep(0, null, 0, listOf(CookieAnimation({
+								visible()
+								outOfScreen(Gravity.TOP)
+							}))),
+							AnimationStep(700, null, 3000, CookieAnimation({
+								topOfItsParent(0f)
+							})),
+							AnimationStep(700, null, 0, CookieAnimation({
+								outOfScreen(Gravity.TOP)
+							}))))
 					.setDuration(5000)
 					.show()
 		}
@@ -29,6 +47,21 @@ class MainActivity : AppCompatActivity() {
 					.setDuration(5000)
 					.setTitle(R.string.bottom_cookie_title)
 					.setIcon(R.mipmap.ic_launcher)
+					.setSteps(listOf(
+							AnimationStep(0, null, 0, CookieAnimation({
+								centerInParent(horizontal = true, vertical = false)
+								invisible()
+							})),
+							AnimationStep(0, null, 0, listOf(CookieAnimation({
+								visible()
+								outOfScreen(Gravity.BOTTOM)
+							}))),
+							AnimationStep(700, null, 3000, CookieAnimation({
+								bottomOfItsParent(0f)
+							})),
+							AnimationStep(700, null, 0, CookieAnimation({
+								outOfScreen(Gravity.BOTTOM)
+							}))))
 					.setMessage(R.string.bottom_cookie_message)
 					.setTitleColor(R.color.yellow)
 					.setCookiePosition(BOTTOM)
@@ -66,6 +99,31 @@ class MainActivity : AppCompatActivity() {
 					.setIcon(R.drawable.cookiemonster)
 					.setCookiePosition(BOTTOM)
 					.setDuration(3000)
+					.setSteps(listOf(
+							AnimationStep(0, null, 0, CookieAnimation({
+								centerInParent(horizontal = true, vertical = false)
+								invisible()
+							}), CookieAnimation({
+								textSize(36f)
+							}, target = com.inlacou.cookiebar3.R.id.tv_title)),
+							AnimationStep(0, null, 0, listOf(CookieAnimation({
+								visible()
+								outOfScreen(Gravity.BOTTOM)
+							}), CookieAnimation({
+								textSize(36f)
+							}, target = com.inlacou.cookiebar3.R.id.tv_title))),
+							AnimationStep(700, AccelerateDecelerateInterpolator(), 5000, CookieAnimation({
+								centerInParent(horizontal = false, vertical = true)
+							})),
+							AnimationStep(700, null, 3000, CookieAnimation({
+								width(505, keepRatio = true, toDp = true)
+								bottomOfItsParent(60f)
+							}), CookieAnimation({
+								textSize(30f)
+							}, target = com.inlacou.cookiebar3.R.id.tv_title)),
+							AnimationStep(700, null, 0, CookieAnimation({
+								outOfScreen(Gravity.BOTTOM)
+							}))))
 					.setShownListener { Log.d("example_activity", "cookie shown") }
 					.setDismissListener { Log.d("example_activity", "cookie dismissed") }
 					.show()
